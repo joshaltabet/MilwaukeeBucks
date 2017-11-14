ce5c576b0f8b077e88fb876ca0b81ff6812d1eb6
Josh Altabet <JoshAltabet@engr2-2-74-255-dhcp.int.colorado.edu>
2017-11-13 18:42:13 -0700

    Delete old files

--- MilwuakeeBucks/MilwaukeeBucks/DevicesTableViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DevicesTableViewController.swift
-//  MilwaukeeBucks
-//
-//  Created by Josh Altabet on 11/12/17.
-//  Copyright © 2017 Josh Altabet. All rights reserved.
-//
-
-import Foundation

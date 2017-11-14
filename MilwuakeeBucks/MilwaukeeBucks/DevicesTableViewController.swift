84fe924a3fc49d5532b3b096a3587e121856271a
Josh Altabet <JoshAltabet@engr2-2-74-255-dhcp.int.colorado.edu>
2017-11-13 18:35:09 -0700

    Initial base code including BlueST SDK and Example

---
+++ MilwuakeeBucks/MilwaukeeBucks/DevicesTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DevicesTableViewController.swift
+//  MilwaukeeBucks
+//
+//  Created by Josh Altabet on 11/12/17.
+//  Copyright © 2017 Josh Altabet. All rights reserved.
+//
+
+import Foundation
